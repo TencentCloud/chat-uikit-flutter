e8659b6e4d1e707788956b55f1a46ad16be74a5e
anonymous <anonymous>
2022-11-17 13:41:46 +0800

    update flutter uikit to 2.0.0

---
+++ lib/ui/views/TIMUIKitChat/TIMUIKItMessageList/utils.dart
@@ -0,0 +1,13 @@
+import 'package:tim_ui_kit/tim_ui_kit.dart';
+
+class TIMUIKitChatUtils {
+  static String? getMessageIDWithinIndex(
+      List<V2TimMessage?> messageList, int index) {
+    if (messageList[index]!.elemType == 11) {
+      if (index > 0) {
+        return getMessageIDWithinIndex(messageList, index - 1);
+      }
+    }
+    return messageList[index]!.msgID;
+  }
+}
